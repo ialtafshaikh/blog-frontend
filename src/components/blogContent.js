0b82c11e6b5c80bd1faf9891ce5795082eed7307
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-11-23 23:11:35 +0530

    created blogContent component

---
+++ src/components/blogContent.js
@@ -0,0 +1,11 @@
+export const blogContent = (content) => {
+  const div = document.createElement("div");
+  div.classList.add("blog-content");
+
+  const p = document.createElement("p");
+  p.textContent(content);
+
+  div.appendChild(p);
+
+  return div;
+};
