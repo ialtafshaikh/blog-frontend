355b58e1b7b9f9732bd6b8b22a5dd42730471cfb
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-11-24 09:58:23 +0530

    added findBlogbyID into helper folder

--- src/components/renderNewBlog.js
+++ src/components/renderNewBlog.js
@@ -1,5 +1,5 @@
 import { blogDetail } from "./blogDetail.js";
-import { findBlogById } from "./findBlogById.js";
+import { findBlogById } from "../helper/findBlogById.js";
 
 export const renderNewBlog = (blogID) => {
   const blog = findBlogById(blogID);
