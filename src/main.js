5dd2261140a1fb4608228ccf6557640516bc4e85
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-11-30 17:48:46 +0530

    added dark mode to the blog

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { Blogs } from "./data.js";
 import { renderNewBlog } from "./components/renderNewBlog.js";
 import { handleClick } from "./helper/handleClick.js";
+import { toggleTheme } from "./helper/darkTheme.js";
 
 window.onload = () => {
   renderNewBlog("2rvqpdbpka3n3fhe");
   document
     .getElementsByClassName("related-link-container")[0]
     .addEventListener("click", handleClick);
+  document.getElementById("slider").addEventListener("change", toggleTheme);
 };
+
+// https://niravkpatel28.github.io/json-data-server/blogs/blogs.json
