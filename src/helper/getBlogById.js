31919d46c5066bac7265e330f82bab2cb2a9d112
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-19 19:28:02 +0530

    removed unnecessary files

--- src/helper/getBlogById.js
+++
@@ -1,7 +0,0 @@
-import { endpoint } from "../endpoints.js";
-
-export const getBlogById = async (blogID) => {
-  let response = await fetch(endpoint + blogID);
-  console.log(response);
-  return response;
-};
