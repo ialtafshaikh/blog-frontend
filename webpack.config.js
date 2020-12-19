6f96f738b0d0030f8676187983d756c4938a97a1
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-19 14:29:27 +0530

    clean webpack plugon installed and configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
-    filename: path.join(__dirname, "src", "main.js"),
+    index: path.join(__dirname, "src", "main.js"),
   },
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "./src/[name].[contenthash].bundle.js",
   },
   module: {
     rules: [
@@ -40,6 +41,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "style.css",
     }),
+    new CleanWebpackPlugin(),
   ],
   mode: "development",
 };
